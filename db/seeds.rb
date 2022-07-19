62130c24879c4822f622c3bd34bb98d2247bd14f
Aaron Lambley <aaron.lambley@gmail.com>
2022-07-19 19:40:32 +0100

    add seed file and populated database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating new database from db/seeds.rb"
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  review_range = rand(1..5)
+  review_range.times do
+    review = Review.create(
+      rating: rand(0..5),
+      content: "#{Faker::Lorem.words(number: rand(3..8)).join(" ")} #{Faker::Lorem.multibyte}",
+      restaurant_id: restaurant.id
+    )
+  end
+  puts "created restaurant with id #{restaurant.id} and #{review_range} review(s)."
+end
