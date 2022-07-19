da149e8bc161b13b8195c0f07026545c1d91a88a
Aaron Lambley <aaron.lambley@gmail.com>
2022-07-19 19:14:52 +0100

    create model validations and associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+  has_many :reviews, dependent: :destroy
+  validates_presence_of :name, :address
+  validates :category, inclusion: CATEGORIES
 end
